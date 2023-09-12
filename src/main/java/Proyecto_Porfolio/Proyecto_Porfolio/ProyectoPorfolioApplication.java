c6cd9d96573300675536a10d1b94bdfd8f66876e
Chachi20 <santanahormigaencarna@gmail.com>
2023-09-12 22:59:34 +0100

    comienzo

---
+++ src/main/java/Proyecto_Porfolio/Proyecto_Porfolio/ProyectoPorfolioApplication.java
@@ -0,0 +1,13 @@
+package Proyecto_Porfolio.Proyecto_Porfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoPorfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoPorfolioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Proyecto_Porfolio/Proyecto_Porfolio/ProyectoPorfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package Proyecto_Porfolio.Proyecto_Porfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoPorfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
